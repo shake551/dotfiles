49ac72e720e3947c107350b22996bb23f7041c04
shake551 <hiroki.yamako@gmail.com>
2023-06-10 11:31:36 +0900

    fix snippet

--- .config/nvim/lua/comp.lua
+++ .config/nvim/lua/comp.lua
@@ -31,6 +31,11 @@ cmp.setup({
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({select = true}) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end
+    },
     sources = cmp.config.sources({
         {name = 'nvim_lsp'}, {name = 'luasnip'}, {name = 'git'}
     }, {{name = 'buffer'}})
